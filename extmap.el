c7af95865ef083327d299b86c254ca87fa88a504
Paul Pogonyshev <pogonyshev@gmail.com>
2023-09-07 21:59:08 +0200

    Release extmap 1.3

--- extmap.el
+++ extmap.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    1.2.1
+;; Version:    1.3
 ;; Keywords:   lisp
 ;; Homepage:   https://github.com/doublep/extmap
 ;; Package-Requires: ((emacs "24.4"))
